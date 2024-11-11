05f2384305e95db7f3810e7c753af9b1c670862d
Solrikuriku <1999.tunglio@gmail.com>
2024-11-12 01:58:41 +0300

    i wanna sleep

--- new_test_avalonia/ViewModels/MainViewModel.cs
+++ new_test_avalonia/ViewModels/MainViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace new_test_avalonia.ViewModels;
+﻿using ReactiveUI;
+using System.ComponentModel;
+using System;
+using System.Reactive;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace new_test_avalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ICommand WriteNum { get; set; }
+   
+    public string Message
+    {
+        get => "1";
+    }
 }
---
+++ new_test_avalonia/ViewModels/NumberViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_test_avalonia.ViewModels
+{
+    internal class NumberViewModel : ViewModelBase
+    {
+        //public string Description { get; set; }
+    }
+}
