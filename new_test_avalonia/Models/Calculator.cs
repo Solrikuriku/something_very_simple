51d2bdad78dffd624be8ac2b2af73c7025680fec
Solrikuriku <1999.tunglio@gmail.com>
2024-11-26 05:33:44 +0300

    add class in model for postfix calc

--- new_test_avalonia/Models/Calculator.cs
+++ new_test_avalonia/Models/Calculator.cs
@@ -8,6 +8,78 @@ namespace new_test_avalonia.Models
 {
     //возможно, удалю
     
+    public class Expr
+    {
+        private string? _infixExpr;
+        private StringBuilder _postfixExpr = new();
+        private Dictionary<char, int> _priority = new()
+        {
+            {'+', 1},
+            {'-', 1},
+            {'*', 2},
+            {'/', 2}
+        };
+
+        public Expr(string expression)
+        {
+            _infixExpr = expression;
+            GetPostfixExpr(expression, ref _postfixExpr);
+        }
+        private StringBuilder GetPostfixExpr(string exp, ref StringBuilder postfix)
+        {
+            var _operandStack = new Stack<Char>();
+            var currentPriority = -1;
+            
+            //нет смысла делать ловушки для дурака, так как текстбокс не пропустит ошибочные символы
+
+            /*
+             * Если символ последний, то он добавляется, а затем все оставшееся из стека.
+             * Если число, то просто добавляется.
+             * Если операнд, то определяется его приоритет.
+             */
+            for(int i = 0; i < exp.Length; i++)
+            {
+                if (i == exp.Length - 1)
+                {
+                    postfix.Append(exp[i]);
+                    CleanStack(ref _operandStack, ref postfix);
+                }
+                else if (Char.IsDigit(exp[i]))
+                    postfix.Append(exp[i]);
+                else
+                {
+                    if (currentPriority == -1 || _priority[exp[i]] > currentPriority)
+                    {
+                        currentPriority = _priority[exp[i]];
+                    }
+                    else
+                    {
+                        //while (_operandStack.Count > 0)
+                        //{
+                        //    postfix.Append(_operandStack.Last());
+                        //    _operandStack.Pop();
+                        //}
+
+                        CleanStack(ref _operandStack, ref postfix);
+                    }
+
+                    _operandStack.Push(exp[i]);
+                }
+            }
+            
+            return postfix;
+        }
+
+        private void CleanStack(ref Stack<char> _opStack, ref StringBuilder pstfx)
+        {
+            while (_opStack.Count > 0)
+            {
+                pstfx.Append(_opStack.Last());
+                _opStack.Pop();
+            }
+        }
+    }
+
     //class Calculator
     //{
     //    private double _firstValue = 0;
